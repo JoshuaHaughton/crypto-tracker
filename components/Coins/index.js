47d639cb5cc3c2472f67fe914ed435131d3054b7
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-04-15 17:30:18 -0400

    Render individual crypto data using Coin component

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import styles from './Coins.module.css'
 
-const Coin = ({name}) => {
+const Coin = ({ name, price, symbol, marketcap, volume, image, priceChange, id }) => {
+
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.coin}>
+          <img src={image} alt={`${name} image`} className={styles.coin_image} />
+          <h1 className=''></h1>
+          <p className={styles.symbol}>{symbol}</p>
+        </div>
+        <div className={styles.data}>
+          <p className={styles.price}>${price}</p>
+          <p className={styles.volume}>${volume.toLocaleString()}</p>
+          {priceChange < 0 ? (
+            <p className={styles.red_percent}>{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className={styles.green_percent}>{priceChange.toFixed(2)}%</p>
+          )
+          }
+
+          <p className={styles.market_cap}>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
