5a76e0101c4382359bb64be371d8cf827742dc43
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-04-16 19:03:55 -0400

    Add search functionality

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,52 @@
-import CoinList from '../components/CoinList'
-import SearchBar from '../components/SearchBar'
-import styles from './Home.module.css'
+import { useEffect } from "react";
+import { useState } from "react";
+import CoinList from "../components/CoinList";
+import SearchBar from "../components/SearchBar";
+import styles from "./Home.module.css";
 
-export default function Home({filteredCoins}) {
-  console.log(filteredCoins)
+export default function Home({ filteredCoins }) {
+  console.log(filteredCoins);
+  const [search, setSearch] = useState("");
+  const [allCoins, setAllCoins] = useState(
+    filteredCoins.filter((coin) => {
+      return coin.name.toLowerCase().includes(search.toLowerCase());
+    }),
+  );
+
+  useEffect(() => {
+    setAllCoins(
+      filteredCoins.filter((coin) => {
+        return coin.name.toLowerCase().includes(search.toLowerCase());
+      }),
+    );
+  }, [search, filteredCoins]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log("change");
+    console.log(e.target.value);
+
+    setSearch(e.target.value.toLowerCase());
+  };
   return (
-      <div className={styles.container}>
-        <h1>Home</h1>
-        <SearchBar type="text" placeholder="Search" />
-        <CoinList filteredCoins={filteredCoins} />
-      </div>
-  )
+    <div className={styles.container}>
+      <h1>Home</h1>
+      <SearchBar type="text" placeholder="Search" onChange={handleChange} />
+      <CoinList filteredCoins={allCoins} />
+    </div>
+  );
 }
 
 export const getServerSideProps = async () => {
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=10&page=1&sparkline=false');
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=10&page=1&sparkline=false",
+  );
 
-  const filteredCoins = await res.json()
+  const filteredCoins = await res.json();
 
   return {
     props: {
-      filteredCoins
-    }
-  }
-
-
-}
+      filteredCoins,
+    },
+  };
+};
