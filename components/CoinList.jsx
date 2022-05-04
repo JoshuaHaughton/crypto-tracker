7d59e66ea410eb6f678839d9801b6fb1a185e4db
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-05-03 23:39:11 -0400

    Update css for home page and coin info page to be responsive on all devices

--- components/CoinList.jsx
+++ components/CoinList.jsx
@@ -1,29 +1,64 @@
-import React from 'react'
-import Coin from './Coins/Coin'
+import React, { useState, useCallback, useEffect } from 'react'
+import Coin, { useMediaQuery } from './Coins/Coin'
 import styles from './CoinList.module.css'
 
 
-const CoinList = ({filteredCoins}) => {
+
+const bigNumberFormatter = (num) => {
+  if (num > 999 && num < 1000000) {
+    return (num / 1000).toFixed(1) + "K"; // convert to K for numbers > 1000 < 1 million
+  } else if (num > 1000000 && num < 1000000000) {
+    return (num / 1000000).toFixed(1) + "M"; // convert to M for numbers > 1 million
+  } else if (num > 1000000000 && num < 1000000000000) {
+    return (num / 1000000000).toFixed(1) + "B"; // convert to B for numbers > 1 billion
+  } else if (num > 1000000000000) {
+    return (num / 1000000000000).toFixed(1) + "T"; // convert to T for numbers > 1 trillion
+  } else if (num <= 999) {
+    return num; // if value < 1000, nothing to do
+  }
+};
+
+const CoinList = ({ filteredCoins }) => {
+
+  const isBreakpoint680 = useMediaQuery(680)
+  const isBreakpoint300 = useMediaQuery(300)
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.name_header}>
           <p>Name</p>
         </div>
-        <div className={styles.price_header}>
-          <p>Price</p>
-        </div>
-        <div className={styles.volume_header}>
+        {!isBreakpoint300 && 
+          <div className={styles.price_header}>
+            <p>Price</p>
+          </div>
+        }
+        {!isBreakpoint680 && <div className={styles.volume_header}>
           <p>Volume</p>
-        </div> 
-        <div className={styles.dayChange_header}>
+        </div>  }
+
+        {!isBreakpoint300 && <div className={styles.dayChange_header}>
           <p>24 Hr Change</p>
-        </div>
-        <div className={styles.marketCap_header}>
+        </div>}
+        
+        
+        {!isBreakpoint680 && <div className={styles.marketCap_header}>
           <p>Market Cap</p>
-        </div>
+        </div>}
       </header>
       {filteredCoins.map(coin => {
+        // let transformedMarketCap = null
+        // let transformedVolume = null
+
+        // if (isBreakpoint) {
+        //   transformedMarketCap = coin.market_cap.toLocaleString();
+        //   transformedVolume = coin.total_volume.toLocaleString();
+        // } else {
+        //   transformedMarketCap = bigNumberFormatter(coin.market_cap)
+        //   transformedVolume = bigNumberFormatter(coin.total_volume)
+        // }
+
         return (
           <Coin 
             key={coin.id}
--- components/Coins/Coin.jsx
+++ components/Coins/Coin.jsx
@@ -1,6 +1,47 @@
+import React, { useState, useCallback, useEffect } from 'react'
 import Link from "next/link";
-import React from "react";
 import styles from "./Coin.module.css";
+
+const bigNumberFormatter = (num) => {
+  if (num > 999 && num < 1000000) {
+    return (num / 1000).toFixed(1) + "K"; // convert to K for numbers > 1000 < 1 million
+  } else if (num > 1000000 && num < 1000000000) {
+    return (num / 1000000).toFixed(1) + "M"; // convert to M for numbers > 1 million
+  } else if (num > 1000000000 && num < 1000000000000) {
+    return (num / 1000000000).toFixed(1) + "B"; // convert to B for numbers > 1 billion
+  } else if (num > 1000000000000) {
+    return (num / 1000000000000).toFixed(1) + "T"; // convert to T for numbers > 1 trillion
+  } else if (num <= 999) {
+    return num; // if value < 1000, nothing to do
+  }
+};
+
+
+export const useMediaQuery = (width) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addEventListener("change", updateTarget)
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeEventListener("change", updateTarget);
+  }, []);
+
+  return targetReached;
+};
 
 const Coin = ({
   name,
@@ -12,6 +53,10 @@ const Coin = ({
   priceChange,
   id,
 }) => {
+
+  const isBreakpoint1250 = useMediaQuery(1250)
+  const isBreakpoint680 = useMediaQuery(680)
+  
   return (
     <Link href='/coin/[id]' as={`coin/${id}`} >
       <div className={styles.container}>
@@ -27,16 +72,36 @@ const Coin = ({
           </div>
    
             <p className={styles.price}>${price}</p>
-            <p className={styles.volume}>${volume.toLocaleString()}</p>
+
+
+
+{!isBreakpoint680 &&
+
+(isBreakpoint1250 ? <p className={styles.volume}>${bigNumberFormatter(volume)}</p> : <p className={styles.volume}>${volume.toLocaleString()}</p>)
+
+}
+
+
+            
+            {/* <p className={styles.volume}>${volume}</p> */}
             {priceChange < 0 ? (
               <p className={styles.red_percent}>{priceChange.toFixed(2)}%</p>
             ) : (
               <p className={styles.green_percent}>{priceChange.toFixed(2)}%</p>
             )}
 
-            <p className={styles.market_cap}>
-              Mkt Cap: ${marketcap.toLocaleString()}
-            </p>
+{!isBreakpoint680 &&  
+
+
+(isBreakpoint1250 ? <p className={styles.market_cap}>${bigNumberFormatter(marketcap)}</p> : <p className={styles.market_cap}>${marketcap.toLocaleString()}</p>)
+
+
+}
+
+
+            {/* <p className={styles.market_cap}>
+              Mkt Cap: ${marketcap}
+            </p> */}
 
         </div>
     </Link>
