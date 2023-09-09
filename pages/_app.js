1e5114905a80268992aa3afa223b29b9de4ad23e
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2023-09-09 09:58:28 -0400

    refactor: move getInitialProps to getServerSideProps func for index

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ Router.events.on("routeChangeComplete", () => {
   });
 });
 
-function MyApp({ Component, pageProps, appProps }) {
+function MyApp({ Component, pageProps }) {
   const isBreakpoint680 = useMediaQuery(680);
   const isBreakpoint380 = useMediaQuery(380);
   const isBreakpoint1040 = useMediaQuery(1040);
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps, appProps }) {
 
   return (
     <Provider store={store}>
-      <Layout coins={appProps}>
+      <Layout>
         <Component
           {...pageProps}
           isBreakpoint380={isBreakpoint380}
@@ -51,73 +51,3 @@ function MyApp({ Component, pageProps, appProps }) {
 }
 
 export default MyApp;
-
-MyApp.getInitialProps = async (appContext) => {
-  const apiKey = process.env.NEXT_PUBLIC_CRYPTOCOMPARE_API_KEY;
-  const fetchOptions = {
-    headers: {
-      Authorization: `Apikey ${apiKey}`,
-    },
-  };
-
-  try {
-    // Fetching all available rates from CryptoCompare's price multi-full endpoint for CAD
-    const exchangeRateResponse = await fetch(
-      `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=CAD&tsyms=USD,AUD,GBP`,
-      fetchOptions,
-    );
-    const exchangeData = await exchangeRateResponse.json();
-
-    const rates = {
-      CAD: 1,
-      USD: exchangeData.RAW.CAD.USD.PRICE,
-      AUD: exchangeData.RAW.CAD.AUD.PRICE,
-      GBP: exchangeData.RAW.CAD.GBP.PRICE,
-    };
-
-    // Fetching the top 100 assets by market cap from CryptoCompare in CAD
-    const assetsResponse = await fetch(
-      "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=CAD",
-      fetchOptions,
-    );
-    const assetsData = await assetsResponse.json();
-
-    const initialHundredCoins = assetsData.Data.map((entry, i) => {
-      const coin = entry.CoinInfo;
-      const metrics = entry.RAW?.CAD;
-      if (!metrics) {
-        console.warn(`Metrics not found for coin: ${coin.Name}`);
-        return null;
-      }
-
-      return {
-        id: coin.Name,
-        symbol: coin.Name,
-        name: coin.FullName,
-        image: `https://cryptocompare.com${coin.ImageUrl}`,
-        current_price: metrics.PRICE,
-        market_cap: metrics.MKTCAP,
-        market_cap_rank: i + 1,
-        total_volume: metrics.TOTALVOLUME24HTO,
-        high_24h: metrics.HIGH24HOUR,
-        low_24h: metrics.LOW24HOUR,
-        price_change_24h: metrics.CHANGE24HOUR,
-        price_change_percentage_24h: metrics.CHANGEPCT24HOUR,
-        circulating_supply: metrics.SUPPLY,
-      };
-    }).filter(Boolean);
-
-    const trendingCoins = initialHundredCoins.slice(0, 10);
-    console.log(initialHundredCoins[0]);
-
-    return {
-      appProps: {
-        initialHundredCoins,
-        trendingCoins,
-        rates,
-      },
-    };
-  } catch (err) {
-    console.log(err);
-  }
-};
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,17 +2,8 @@ import React from "react";
 import Head from "next/head";
 import styles from "./Layout.module.css";
 import Navbar from "../Navbar/Navbar";
-import { coinsActions } from "../../store/coins";
-import { useDispatch } from "react-redux";
-export const Layout = ({ children, title = "Crypto Tracker", coins }) => {
-  const dispatch = useDispatch();
-  // dispatch(coinsActions.updateCoins({initialHundredCoins: coins.initialHundredCoins, trendingCoins: coins.trendingCoins, symbol: "$"}));
 
-  //sets coins to redux state before beginning to render page component (Children) so that there wont be a loading state on intial load
-  dispatch(coinsActions.updateCoins({ ...coins, symbol: "$" }));
-
-  // const [layoutCoins, setLayoutCoins] = useState(coins)
-
+export const Layout = ({ children, title = "Crypto Tracker" }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,33 +14,7 @@ export const Layout = ({ children, title = "Crypto Tracker", coins }) => {
 
       <Navbar />
 
-      <main>
-        {/* {children} */}
-        {React.cloneElement(children, { coins })}
-        {/* <footer className={styles.footer}>
-            <div className={styles.footer_links}>
-              <Link href="/" className={styles.footer_link}>
-                  <a >Home</a>
-              </Link>
-
-              <Link href="/" className={styles.footer_link}>
-                  <a >News</a>
-              </Link>
-
-              <Link href="/" className={styles.footer_link}>
-                  <a >Portfolio</a>
-              </Link>
-
-              <Link href="/" className={styles.footer_link}>
-                  <a >Login</a>
-              </Link>
-
-              <Link href="/">
-                  <a className={styles.footer_link}>Logout</a>
-              </Link>
-            </div>
-          </footer> */}
-      </main>
+      <main>{children}</main>
     </div>
   );
 };
