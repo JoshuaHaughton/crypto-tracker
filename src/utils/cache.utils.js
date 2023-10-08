29de313aba8b662fd2a1e36d388b0bf4ec5ae4d3
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2023-10-07 20:01:45 -0400

    refactor: remove localStorage logic

--- src/utils/cache.utils.js
+++ src/utils/cache.utils.js
@@ -14,51 +14,6 @@ import { initializeCoinListCache } from "../thunks/coinListCacheThunk";
 import { updateStoreData } from "./store.utils";
 
 /**
- * Marks a property in the localStorage as valid with an expiration timestamp.
- * @param {string} tableName - The name of the table in the IndexedDB.
- * @param {string} key - The key under which the value should be stored.
- */
-export const setToLocalStorageWithExpiry = (tableName, key) => {
-  const now = Date.now();
-  const expiry = now + CACHE_EXPIRY_TIME_IN_MINUTES * 60 * 1000;
-  localStorage.setItem(
-    `${tableName}_${key}`,
-    JSON.stringify({ value: "valid", expiry }),
-  );
-};
-
-/**
- * Retrieves a value from the localStorage. If the value has expired, it will be removed.
- * @param {string} tableName - The name of the table in the IndexedDB.
- * @param {string} key - The key under which the value is stored.
- * @returns {any|null} - The stored value or null if not found or expired.
- */
-export const getFromLocalStorageWithExpiryCheck = (tableName, key) => {
-  const itemStr = localStorage.getItem(`${tableName}_${key}`);
-  if (!itemStr) return null;
-
-  const { value, expiry } = JSON.parse(itemStr);
-  if (Date.now() > expiry) {
-    localStorage.removeItem(`${tableName}_${key}`);
-    return null;
-  }
-  return value;
-};
-
-/**
- * Checks if the cache for a specific table and key is valid.
- * @param {string} tableName - The name of the table in the IndexedDB.
- * @returns {boolean} - Indicates whether the cache is valid or not.
- */
-export const isCacheValid = (tableName) => {
-  const currencies = ALL_CURRENCIES;
-  return currencies.every(
-    (currency) =>
-      getFromLocalStorageWithExpiryCheck(tableName, currency) === "valid",
-  );
-};
-
-/**
  * Clears cache for a specific table and key.
  * @param {string} tableName - The name of the table in the IndexedDB.
  * @param {string} key - The key for which to clear the cache.
@@ -73,9 +28,6 @@ export const clearCache = async (tableName, key) => {
       err,
     );
   }
-
-  localStorage.removeItem(`${tableName}_${key}`);
-  console.log(`Cache cleared from localStorage for ${key} in ${tableName}`);
 };
 
 /**
@@ -92,14 +44,6 @@ export const clearCacheForAllKeysInTable = async (tableName) => {
       err,
     );
   }
-
-  // Clear from localStorage
-  for (const key in localStorage) {
-    if (key.startsWith(`${tableName}_`)) {
-      localStorage.removeItem(key);
-    }
-  }
-  console.log(`Cache cleared from localStorage for all keys in ${tableName}`);
 };
 
 /**
@@ -122,13 +66,13 @@ export const fetchDataFromIndexedDB = async (tableName, key) => {
 };
 
 /**
- * Saves coin data to IndexedDB for a specific currency and sets its expiry in localStorage.
+ * Saves coin data to IndexedDB for a specific currency.
  *
  * @param {string} tableName - The name of the table in the IndexedDB where the coin data should be stored.
  * @param {string} currency - The currency identifier under which the coin data should be stored.
  * @param {Object} coinData - The data related to the specified currency that needs to be stored.
  *
- * @returns {Promise<void>} - Resolves when the data is successfully saved and its expiry is set in localStorage.
+ * @returns {Promise<void>} - Resolves when the data is successfully saved.
  */
 export const saveCoinDataForCurrencyInBrowser = async (
   tableName,
@@ -162,13 +106,9 @@ export const saveCoinDataForCurrencyInBrowser = async (
     } else if (tableName === CURRENCYRATES_TABLENAME) {
       await db[tableName].put({ currency, rates: coinData });
     }
-    setToLocalStorageWithExpiry(tableName, currency);
-    console.log(
-      `Successfully saved ${currency} data to ${tableName} in IndexedDB and set expiry in localStorage.`,
-    );
   } catch (err) {
     console.error(
-      `Error saving data to IndexedDB for ${currency} in ${tableName} and setting expiry in localStorage`,
+      `Error saving data to IndexedDB for ${currency} in ${tableName}`,
       err,
     );
   }
@@ -315,7 +255,7 @@ export const areNecessaryCachesValid = async (serverGlobalCacheVersion) => {
 };
 
 /**
- * Clears cache from local storage, indexedDB, and cookies.
+ * Clears cache from indexedDB, and cookies.
  */
 export const clearAllCaches = async () => {
   console.warn("CLEARING ALL CACHES");
