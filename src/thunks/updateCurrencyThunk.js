75c2cb8c4b5355a433795260c1abbde4f6fed697
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2023-10-09 20:37:40 -0400

    refactor: clean up updateCurrency logic

--- src/thunks/updateCurrencyThunk.js
+++ src/thunks/updateCurrencyThunk.js
@@ -4,9 +4,14 @@ import { currencyActions } from "../store/currency";
 import { postMessageToCurrencyTransformerWorker } from "../utils/currencyTransformerService";
 import Cookies from "js-cookie";
 import { CURRENT_CURRENCY_COOKIE_EXPIRY_TIME } from "../global/constants";
+import { isEmpty } from "lodash";
 
 /**
  * Thunk to update currency.
+ * @param {Object} payload - The payload containing the currency data.
+ * @param {string} payload.currency - The updated currency value.
+ * @returns {Promise<void>} - Resolves after all actions are dispatched.
+ *
  * Uses a web worker to perform the currency transformation for a list of coins.
  * After receiving the transformed data from the web worker, dispatches the results to the Redux store.
  */
@@ -16,8 +21,6 @@ export const updateCurrency = createAsyncThunk(
     console.log("updateCurrencyThunk active", payload);
 
     const { currency: updatedCurrency } = payload;
-    const state = getState();
-
     const {
       coins: {
         displayedCoinListCoins,
@@ -26,56 +29,47 @@ export const updateCurrency = createAsyncThunk(
         cachedCoinDetailsByCurrency,
       },
       currency: { currentCurrency, currencyRates: initialRates },
-    } = state;
+    } = getState();
 
     // Update cookie immediately
     Cookies.set("currentCurrency", updatedCurrency, {
       expires: CURRENT_CURRENCY_COOKIE_EXPIRY_TIME,
     });
 
-    // handle selected coin details transformations if needed. We can prioritize these since if they exist,
-    // we aren't on the page with coin list coins anyways
-    if (Object.keys(selectedCoinDetails).length > 0) {
-      if (
-        cachedCoinDetailsByCurrency[updatedCurrency][
-          selectedCoinDetails.coinInfo.symbol.toUpperCase()
-        ]
-      ) {
-        console.log("CACHE USED - COIN DETAILS");
-        // Dispatch the cached data
+    const updateCurrencyAndCache = (type, coins, cache) => {
+      if (cache && !isEmpty(cache)) {
+        console.log(`CACHE USED - COIN ${type.toUpperCase()}`);
         dispatch(
-          coinsActions.updateSelectedCoin({
-            coinDetails:
-              cachedCoinDetailsByCurrency[updatedCurrency][
-                selectedCoinDetails.coinInfo.symbol.toUpperCase()
-              ],
-          }),
+          type === "Details"
+            ? coinsActions.updateSelectedCoin({ coinDetails: cache })
+            : coinsActions.updateCoins({
+                displayedCoinListCoins: cache,
+                trendingCarouselCoins: cache.slice(0, 10),
+              }),
         );
-
         // Update the currency state after all coins have been updated
         dispatch(currencyActions.changeCurrency(payload));
       } else {
         // We update the currency after the transformations when not using the cache iside of the transformerworker so that we call it as soon as it's ready
-
-        // CoinDetails Cache doesn't exist
-        console.log("CACHE NOT USED - COIN DETAILS");
-        console.log("why cach wasnt used:", cachedCoinDetailsByCurrency);
-        console.log("selectedCoinDetails", selectedCoinDetails);
+        console.log(`CACHE NOT USED - COIN ${type.toUpperCase()}`);
+        console.log(`CACHE NOT USED - COIN`, coins);
         // Only transform the requested currency first to save time. Then, we cache the rest
         postMessageToCurrencyTransformerWorker({
-          type: "transformCoinDetailsCurrency",
+          type: `transformCoin${type}Currency`,
           data: {
-            coinToTransform: selectedCoinDetails,
+            [type === "Details" ? "coinToTransform" : "coinsToTransform"]:
+              coins,
             fromCurrency: currentCurrency.toUpperCase(),
             toCurrency: updatedCurrency.toUpperCase(),
             currencyRates: initialRates,
           },
         });
-        // Re-attempt caching all coins - if one doesn't exist, then we assume there's none that do
+        // Re-attempt caching all coins - if one doesn't exist, then there's a good chance that there's more than one
         postMessageToCurrencyTransformerWorker({
-          type: "transformAllCoinDetailsCurrencies",
+          type: `transformAllCoin${type}Currencies`,
           data: {
-            coinToTransform: selectedCoinDetails,
+            [type === "Details" ? "coinToTransform" : "coinsToTransform"]:
+              coins,
             fromCurrency: currentCurrency.toUpperCase(),
             currencyRates: initialRates,
             // Only transform the ones that haven't been transformed yet
@@ -86,53 +80,24 @@ export const updateCurrency = createAsyncThunk(
           },
         });
       }
+    };
+
+    // Handle selected coin details transformations if needed.
+    if (Object.keys(selectedCoinDetails).length > 0) {
+      const cache =
+        cachedCoinDetailsByCurrency[updatedCurrency]?.[
+          selectedCoinDetails.coinInfo.symbol.toUpperCase()
+        ];
+      console.log("cache", cachedCoinDetailsByCurrency);
+      console.log("cache", cache);
+      updateCurrencyAndCache("Details", selectedCoinDetails, cache);
     }
 
-    // Coinlist Cache exists
-    if (coinListCoinsByCurrency[updatedCurrency].length > 0) {
-      console.log("CACHE USED - COIN LIST");
-      // Dispatch the cached data
-      dispatch(
-        coinsActions.updateCoins({
-          displayedCoinListCoins: coinListCoinsByCurrency[updatedCurrency],
-          trendingCarouselCoins: coinListCoinsByCurrency[updatedCurrency].slice(
-            0,
-            10,
-          ),
-        }),
-      );
-
-      // Update the currency state after all coins have been updated
-      dispatch(currencyActions.changeCurrency(payload));
-    } else {
-      // We update the currency after the transformations when not using the cache iside of the transformerworker so that we call it as soon as it's ready
-
-      // CoinList Cache doesn't exist
-      console.log("CACHE NOT USED - COIN LIST");
-      // Only transform the requested currency first to save time. Then, we cache the rest
-      postMessageToCurrencyTransformerWorker({
-        type: "transformCoinListCurrency",
-        data: {
-          coinsToTransform: displayedCoinListCoins,
-          fromCurrency: currentCurrency.toUpperCase(),
-          toCurrency: updatedCurrency.toUpperCase(),
-          currencyRates: initialRates,
-        },
-      });
-      // Re-attempt caching all coins - if one doesn't exist, then there's a good chance that there's more than one
-      postMessageToCurrencyTransformerWorker({
-        type: "transformAllCoinListCurrencies",
-        data: {
-          coinsToTransform: displayedCoinListCoins,
-          fromCurrency: currentCurrency.toUpperCase(),
-          currencyRates: initialRates,
-          // Only transform the ones that haven't been transformed yet
-          currenciesToExclude: [
-            updatedCurrency.toUpperCase(),
-            currentCurrency.toUpperCase(),
-          ],
-        },
-      });
-    }
+    // Handle coin list transformations
+    updateCurrencyAndCache(
+      "List",
+      displayedCoinListCoins,
+      coinListCoinsByCurrency[updatedCurrency],
+    );
   },
 );
