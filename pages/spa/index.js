90f0942474a23f9b455781d7489ca5c909a079bb
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2023-11-07 09:29:31 -0500

    refactor: clean up new layout

--- pages/spa/index.js
+++ pages/spa/index.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SpaLayout from "../../src/components/SpaLayout/SpaLayout";
 import Dashboard from "../../src/spaRoutes/Dashboard/Dashboard";
 import PopularCoins from "../../src/spaRoutes/PopularCoins/PopularCoins";
 import Portfolio from "../../src/spaRoutes/Portfolio/Portfolio";
 import CoinDetails from "../../src/spaRoutes/CoinDetails/CoinDetails";
-import SpaLayout from "../../src/components/SpaLayout/SpaLayout";
 import { preparePopularCoinsListPageProps } from "../../src/utils/api.server.utils";
 
 const SpaPage = () => {
@@ -11,7 +11,7 @@ const SpaPage = () => {
     <Router>
       <SpaLayout>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/market" element={<PopularCoins />} />
           <Route path="/market/:id" element={<CoinDetails />} />
           <Route path="/portfolio" element={<Portfolio />} />
