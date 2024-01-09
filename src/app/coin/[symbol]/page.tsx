09368c588ae0ffd117273603085e934e1f0ccce9
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2024-01-09 01:24:39 -0500

    refactor: add dynamic export to RSCs

--- src/app/coin/[symbol]/page.tsx
+++ src/app/coin/[symbol]/page.tsx
@@ -1,11 +1,15 @@
 import "server-only";
-
 import CoinDetailsPage from "@/components/pages/CoinDetailsPage/CoinDetailsPage";
 import { useRouter } from "next/router";
 import { cookies } from "next/headers";
 import { fetchCoinDetailsData } from "@/api/fetchCoinDetails";
 import { StoreProvider } from "@/lib/store/storeProvider";
 import { INITIAL_CURRENCY, TCurrencyString } from "@/lib/constants";
+
+// This line sets the rendering behavior of the page to be dynamic for every request.
+// 'force-dynamic' ensures that the page is always rendered on the server side,
+// which is useful for pages that require fresh data from the server on each request/reload.
+export const dynamic = "force-dynamic";
 
 export default async function CoinPage() {
   const router = useRouter();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import "server-only";
-
 import HomePage from "@/components/pages/HomePage/HomePage";
 import { cookies } from "next/headers";
 import { StoreProvider } from "@/lib/store/storeProvider";
 import { fetchPopularCoinsData } from "@/api/fetchPopularCoins";
 import { TCurrencyString, INITIAL_CURRENCY } from "@/lib/constants";
+
+// This line sets the rendering behavior of the page to be dynamic for every request.
+// 'force-dynamic' ensures that the page is always rendered on the server side,
+// which is useful for pages that require fresh data from the server on each request/reload.
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
   // Retrieve the currency preference from cookies
