cf517f3057f2b8fa052b050f52121c69beef06d3
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2024-03-27 05:48:10 -0400

    refactor: update css var declaration

--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,8 +1,9 @@
 const StyleDictionary = require("style-dictionary");
 
-// Function to format the output by categories
-function formatByCategory(allProperties) {
+// Function to format the output by categories and handle specific properties
+function formatByCategory(allProperties, isScss = false) {
   let categories = {};
+  const prefix = isScss ? "$" : "--"; // Set prefix based on SCSS or CSS
 
   // Grouping properties by category
   allProperties.forEach((prop) => {
@@ -15,57 +16,78 @@ function formatByCategory(allProperties) {
 
   // Formatting each category
   let output = "";
-  for (const category in categories) {
-    output += `// ${category.charAt(0).toUpperCase() + category.slice(1)}\n`;
+
+  const categoryKeys = Object.keys(categories);
+  categoryKeys.forEach((category, index) => {
+    output += `\n// ${category.charAt(0).toUpperCase() + category.slice(1)}\n`;
+
     categories[category].forEach((prop) => {
-      output += `  --${prop.name}: ${prop.value};\n`;
+      output += `  ${prefix}${prop.name}: ${prop.value};\n`;
       if (prop.darkValue) {
-        output += `  --${prop.name}-dark: ${prop.darkValue};\n`;
+        output += `  ${prefix}${prop.name}-dark: ${prop.darkValue};\n`;
       }
       if (prop.weight) {
-        // Check if 'prop.name' contains the term 'sizes', replace it with 'weight'
-        const propertyName = prop.name.includes("sizes")
+        const weightPropertyName = prop.name.includes("sizes")
           ? prop.name.replace("sizes", "weight")
           : `${prop.name}-weight`;
-
-        output += `  --${propertyName}: ${prop.weight};\n`;
+        output += `  ${prefix}${weightPropertyName}: ${prop.weight};\n`;
       }
       if (prop.lineHeight) {
-        // Check if 'prop.name' contains the term 'sizes', replace it with 'weight'
-        const propertyName = prop.name.includes("sizes")
+        const lineHeightPropertyName = prop.name.includes("sizes")
           ? prop.name.replace("sizes", "lineHeight")
           : `${prop.name}-lineHeight`;
-
-        output += `  --${propertyName}: ${prop.lineHeight};\n`;
+        output += `  ${prefix}${lineHeightPropertyName}: ${prop.lineHeight};\n`;
       }
     });
-    output += `\n`; // Add a newline for spacing between categories
-  }
+    // Add a newline for spacing between categories only if it's not the last category
+    if (index < categoryKeys.length - 1) {
+      output += `\n`;
+    }
+  });
 
-  return output;
+  return output.trim();
 }
 
-// Register a custom format
+// Register custom formats
 StyleDictionary.registerFormat({
   name: "custom/css/variables",
-  formatter: function ({ dictionary, options }) {
-    let output = `:root {\n`;
-    output += formatByCategory(dictionary.allProperties);
-    output += `}\n`;
-    return output;
+  formatter: function ({ dictionary }) {
+    return `:root {\n${formatByCategory(dictionary.allProperties)}\n}`;
   },
 });
+
+StyleDictionary.registerFormat({
+  name: "custom/scss/variables",
+  formatter: function ({ dictionary }) {
+    return formatByCategory(dictionary.allProperties, true);
+  },
+});
+
+const isColorToken = (prop) => prop.attributes.category === "colors";
+const isNotColorToken = (prop) => prop.attributes.category !== "colors";
 
 module.exports = {
   source: ["src/styles/tokens/*.tokens.json"],
   platforms: {
+    css: {
+      transformGroup: "css",
+      buildPath: "src/styles/partials/",
+      files: [
+        {
+          destination: "_generatedCssVars.scss",
+          format: "custom/css/variables",
+          filter: isNotColorToken,
+        },
+      ],
+    },
     scss: {
       transformGroup: "scss",
       buildPath: "src/styles/partials/",
       files: [
         {
-          destination: "_variables.scss",
-          format: "custom/css/variables",
+          destination: "_generatedScssVars.scss",
+          format: "custom/scss/variables",
+          filter: isColorToken,
         },
       ],
     },
